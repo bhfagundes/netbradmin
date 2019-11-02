e0d535781501976fc915a291dc3628222963d73e
Brenner Fagundes <bhfagundes@gmail.com>
2019-11-02 11:39:35 -0200

    add contact

--- resources/views/site/menu.blade.php
+++ resources/views/site/menu.blade.php
@@ -1,8 +1,8 @@
                                 <div class="navbar-collapse collapse clearfix">
                                     <ul class="navigation clearfix">
                                        <li><a href="/">Início</a></li>
-                                       <li><a href="/planos">Planos</a></li>
-                                       <li><a href="/contato">Contato</a></li>
+                                       <li><a href="planos">Planos</a></li>
+                                       <li><a href="contato">Contato</a></li>
                                         
                                     </ul>
                                 </div>
