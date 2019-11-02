ae5fd5ec161656664c7f528c808c050d7a09cd0b
Brenner Fagundes <bhfagundes@gmail.com>
2019-11-02 11:48:32 -0200

    add contact

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
     return view('site/index');
 });
 
-Route::get('contato', function () {
+Route::get('/contato', function () {
     return view('site/contato');
 });
 
