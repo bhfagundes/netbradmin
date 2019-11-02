878e4b478be81b12752ee136a807d49fa6e5a0fb
Brenner Fagundes <bhfagundes@gmail.com>
2019-11-02 13:44:36 -0200

    fix menu

--- resources/views/site/menu.blade.php
+++ resources/views/site/menu.blade.php
@@ -1,8 +1,8 @@
                                 <div class="navbar-collapse collapse clearfix">
                                     <ul class="navigation clearfix">
-                                       <li><a href="/">Início</a></li>
-                                       <li><a href="/planos">Planos</a></li>
-                                       <li><a href="/contato">Contato</a></li>
+                                       <li><a href="/public">Início</a></li>
+                                       <li><a href="/public/planos">Planos</a></li>
+                                       <li><a href="/public/contato">Contato</a></li>
                                         
                                     </ul>
                                 </div>
